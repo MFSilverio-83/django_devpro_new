f54d3d438fe24efad6551b4f3dbf202c7634d1cf
MFSilverio-83 <mfsilverio@ymail.com>
2024-02-16 12:50:59 -0300

    Primeira_Template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def portao(request):
+    return HttpResponse ("Você chegou no portão!")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
