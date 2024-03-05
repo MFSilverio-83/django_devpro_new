1a3715faa1ee53ea6dcb0425ca8f2b9c1186481e
MFSilverio-83 <mfsilverio@ymail.com>
2024-03-05 11:30:42 -0300

    Django_devpro_new

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post # do arquivo models, importe a tabela post
 # Create your views here.
 
 def portao(request):
     return HttpResponse ("Você chegou no portão!")
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # na linha abaixo serão gravados os post de acordo com data de publicação
+    # posts será uma nova variável criada, que pode ser utilizada em outro local (templates)
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('blog.urls')), # vai buscar a página no app blog
 ]
