0c43c3998f4d64816315f9e6f43e59bb39dd3e56
MFSilverio-83 <mfsilverio@ymail.com>
2024-03-07 11:17:39 -0300

    Add nova viem e template para detalhes dos post e add de arquivos css

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # página inicial
-    path('portao', views.portao),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post # do arquivo models, importe a tabela post
 # Create your views here.
-
-def portao(request):
-    return HttpResponse ("Você chegou no portão!")
 
 def post_list(request):
     # na linha abaixo serão gravados os post de acordo com data de publicação
     # posts será uma nova variável criada, que pode ser utilizada em outro local (templates)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 
 
