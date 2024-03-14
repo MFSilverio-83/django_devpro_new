c0e7acab0069f1bd1c44a29856130f81a6c8c32b
MFSilverio-83 <mfsilverio@ymail.com>
2024-03-14 10:37:14 -0300

    Inclusão de rascunhos e Paginação

--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post # do arquivo models, importe a tabela post
 from .forms import PostForm
-# Create your views here.
+from django.core.paginator import Paginator
 
 def post_list(request):
     # na linha abaixo serão gravados os post de acordo com data de publicação
     # posts será uma nova variável criada, que pode ser utilizada em outro local (templates)
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator = Paginator(posts_list, 5)  # Define o número de posts por página
+    page_number = request.GET.get('page')  # Obtém o número da página da query string
+    posts = paginator.get_page(page_number)  # Obtém os posts para a página atual
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
 
 def post_new(request):
     if request.method == "POST":
@@ -21,13 +23,12 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -36,9 +37,24 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    # garante que pegamos apenas postagens não publicadas e por ordem de criação
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+# def post_remove(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     post.delete()
+#     return redirect('post_list')
